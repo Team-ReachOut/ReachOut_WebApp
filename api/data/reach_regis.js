36d92c630e4355f6b54e2b212ea0f11b52d71bf3
jkks1234 <kaushik.jatin74@gmail.com>
2017-09-21 21:04:37 +0530

    Hospital reg schema added

---
+++ api/data/reach_regis.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+require('mongoose-double')(mongoose);
+var SchemaTypes = mongoose.Schema.Types;
+var reach_regis = new mongoose.Schema(
+{
+	name:
+	{
+		type :String,
+		require :true
+	},
+	phonenumber:
+	{
+		type : Number,
+		require :true
+	},
+
+    address : {
+    	type:String,
+    	require : true
+    
+  	},
+  	city:
+  	{
+  		type : String,
+  		require :true
+  	}
+});
+mongoose.model('reach_regis',reach_regis,'Reach_regis');
